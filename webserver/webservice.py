3447dbaeb7d3a52500a24394bd810dfe18d5c489
eyalorbach <eyal@mac.com>
2019-04-18 15:34:38 +0300

    utf8 header n response

--- webserver/webservice.py
+++ webserver/webservice.py
@@ -8,6 +8,8 @@ from logic import api
 centroids = np.load(conf.centroids_file)
 densitys = np.load(conf.denstitys_file)
 
+API_HEADERS = {'Content-Type': 'application/json; charset=utf-8'}
+
 api.init()
 
 @route('/bots/api/subjectdensity', method=['POST'])
@@ -15,21 +17,21 @@ def subj_density():
     r = request
     data = r.json
     results = api.subject_density(data, centroids, densitys)
-    return HTTPResponse(code=200, body=results)
+    return HTTPResponse(code=200, body=results, headers=API_HEADERS)
 
 @route('/bots/api/tweetsimilarity', method=['POST'])
 def tweet_similarity():
     r = request
     data = r.json
     results = api.tweet_similarity(data)
-    return HTTPResponse(code=200, body=results)
+    return HTTPResponse(code=200, body=results, headers=API_HEADERS)
 
 @route('/bots/api/behavioursimilarity', method=['POST'])
 def tweet_similarity():
     r = request
     data = r.json
     results = api.behavior_similrity(data)
-    return HTTPResponse(code=200, body=results)
+    return HTTPResponse(code=200, body=results, headers=API_HEADERS)
 
 @route('/index', method=['GET'])
 def startapp():
